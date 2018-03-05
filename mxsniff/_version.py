8a9107066a95e5bd3895108b13b2f5a4eff6e94e
Kiran Jonnalagadda <kiran@hasgeek.com>
2018-03-05 14:38:38 +0530

    Release version 0.3.3

--- mxsniff/_version.py
+++ mxsniff/_version.py
@@ -1,4 +1,4 @@
 __all__ = ['__version__', '__version_info__']
 
-__version__ = '0.3.dev3'
+__version__ = '0.3.3'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
